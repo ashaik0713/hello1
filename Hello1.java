087fc7ec2b78a1745ad7ddfd174e866c335379cb
Shaik <amzad.shaik@accenture.com>
2019-02-17 20:00:31 +0530

    first commit

---
+++ Hello1.java
@@ -0,0 +1,14 @@
+//First Program for user input
+
+import java.util.Scanner;
+
+public class Hello1{
+
+    public static void main(String[] args)
+       {
+	System.out.println("Hello dude, Dont Enter some number  : ");
+	}
+   }
+
+
+
