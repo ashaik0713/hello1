8fc407a8b87cbe1571e2ce35c379214c23268b74
Shaik <amzad.shaik@accenture.com>
2019-02-17 15:55:14 +0530

    three files added

---
+++ Userin.java
@@ -0,0 +1,18 @@
+//First Program for user input
+
+import java.util.Scanner;
+
+public class Userin{
+
+    public static void main(String[] args)
+       {
+	Scanner input = new Scanner(System.in);
+	System.out.println("Hello dude, Enter some number  : ");
+	int Var1 =input.nextInt();
+	System.out.println(" Dude! Did you enter this??" + Var1);
+
+	}
+   }
+
+
+
